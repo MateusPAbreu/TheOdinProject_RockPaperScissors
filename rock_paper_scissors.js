fb1113131b6f02b0905142b8457a0600926012ac
MateusPAbreu <mateuspa0204@gmail.com>
2025-03-23 00:20:35 -0300

    Finished adding the logic for the game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,36 +1,51 @@
 let compScore = 0;
 let playerScore = 0;
 
-function computerChoice(){
-    return compChoice = Math.floor(Math.random() *3); //0 to 2
+function computerChoice() {
+    return compChoice = Math.floor(Math.random() * 3); //0 to 2
 }
 
-function humanChoice(choice){
+function humanChoice(choice) {
     //paper = 0, rock = 1, scissors = 2
-    if(choice.toLowerCase() == "paper"){
+    if (choice.toLowerCase() == "paper") {
         //console.log("Paper");
         return 0;
-    } 
-    else if(choice.toLowerCase() == "rock"){
+    }
+    else if (choice.toLowerCase() == "rock") {
         //console.log("Rock");
         return 1;
-    }else{
+    } else {
         //console.log("Scissors");
         return 2;
     }
 }
 
-function gameLogic(comp, player){
-    // if((comp - player) == 1 || (comp - player) == -1){
-    //     if(comp<player){//paper and rock
-    //         compScore = compScore+1;
-    //     }else{//rock scissors
-    //         playerScore = playerScore + 1;
-    //     }
-    // }else{//scissors and paper
-
-    // }
+function gameLogic(player, comp) {
+    console.log("Player: ", player);
+    console.log("Computer: ", comp);
+    //paper, rock; rock, scissors
+    if (player - comp == 1) {
+        playerScore += 1;
+    }
+    else if (player - comp == -1) {
+        compScore += 1;
+    }
+    //paper, scissors
+    else if (player > comp) {
+        playerScore += 1;
+    } 
+    else if (comp > player) {//if I don't do this, the computer wins even when we tie
+        compScore += 1;
+    }
 }
 
-choice = prompt("Choose Rock Paper Scissors:");
-humanChoice(choice);
+for (let i = 0; i < 5; i++) {
+    playerChoice = prompt("Choose Rock Paper Scissors:");
+    player = humanChoice(playerChoice);
+    console.log(player)
+    compChoice = computerChoice();
+    gameLogic(player, compChoice);
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + compScore);
+}
+
